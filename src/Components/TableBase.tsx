944524016c3817c981a248fef450999ff5cd725e
akutori <78586241+akutori@users.noreply.github.com>
2024-04-29 23:21:02 +0900

    使っていない記述をコメントアウトor削除

--- src/Components/TableBase.tsx
+++ src/Components/TableBase.tsx
@@ -1,41 +1,41 @@
-import { fakerJA as faker } from "@faker-js/faker";
-import { Category } from "grapesjs";
-import { useState } from "react";
-import { copyToClipboard } from "Utils/CopyToClipbord";
-import PersonMenu from "Components/PersonMenu";
-import CompanyMenu from "Components/CompanyMenu";
-import GenerateIpsumMenu from "../GenerateIpsumMenu";
+// import { fakerJA as faker } from "@faker-js/faker";
+// import { Category } from "grapesjs";
+// import { useState } from "react";
+// import { copyToClipboard } from "Utils/CopyToClipbord";
+// import PersonMenu from "Components/PersonMenu";
+// import CompanyMenu from "Components/CompanyMenu";
+// import GenerateIpsumMenu from "../GenerateIpsumMenu";
 
-function TableBase() {
+// function TableBase() {
 
-    const [menuData, setMenuData] = useState();
+//     const [menuData, setMenuData] = useState();
 
-    console.log([menuData,]);
+//     console.log([menuData,]);
 
-    function generateData() {
+//     function generateData() {
 
-        // <td><CategoryName /></td>
-        // <td className="genelate-datas"><CategoryData /></td>
-        // <td><button type="button" onClick={() => copyToClipboard(categoryData)}>コピー</button></td>
+//         // <td><CategoryName /></td>
+//         // <td className="genelate-datas"><CategoryData /></td>
+//         // <td><button type="button" onClick={() => copyToClipboard(categoryData)}>コピー</button></td>
 
-    }
+//     }
 
-    return (
-        <table>
-            <thead>
-                <tr>
-                    <th>category</th>
-                    <th>data</th>
-                    <th>button</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    {menuData}
-                </tr>
-            </tbody>
-        </table>
-    )
-}
+//     return (
+//         <table>
+//             <thead>
+//                 <tr>
+//                     <th>category</th>
+//                     <th>data</th>
+//                     <th>button</th>
+//                 </tr>
+//             </thead>
+//             <tbody>
+//                 <tr>
+//                     {menuData}
+//                 </tr>
+//             </tbody>
+//         </table>
+//     )
+// }
 
-export default TableBase;
+// export default TableBase;
